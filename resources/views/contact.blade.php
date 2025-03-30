a9978a28c2b67d207b86f7908ae657d2aab99522
danil <dkalimullin470@gmail.com>
2025-03-30 19:48:11 +0500

    add pages

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    contact
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('index');
 
-Route::get('/courses', fn () => 'courses')->name('course');
+Route::view('/courses', 'courses')->name('course');
 
-Route::get('/contact', fn () => 'contact')->name('contact');
+Route::view('/contact', 'contact')->name('contact');
 
-Route::get('/login', fn () => 'login')->name('login');
+Route::view('/login', 'login')->name('login');
 
-Route::get('/register', fn () => 'register')->name('register');
+Route::view('/register', 'register')->name('register');
