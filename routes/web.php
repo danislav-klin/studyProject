19f794027f628f36c3e4d7c31d63cc7efda9631d
danil <dkalimullin470@gmail.com>
2025-03-30 19:36:34 +0500

    add basic layout template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/', 'index')->name('index');
+
+Route::get('/courses', fn () => 'courses')->name('course');
+
+Route::get('/contact', fn () => 'contact')->name('contact');
+
+Route::get('/login', fn () => 'login')->name('login');
+
+Route::get('/register', fn () => 'register')->name('register');
